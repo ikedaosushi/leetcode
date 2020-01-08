91b60dc6f2f97db8fbb95312eb2e18eb0d504453
Ikeda Yutao <yutaro.ikeda@kaizenplatform.com>
2020-01-08 15:11:32 +0900

    Fix

--- python/climbStairs.py
+++ python/climbStairs.py
@@ -1,13 +1,16 @@
-from functools import lru_cache
-
-
 class Solution:
     def climbStairs(self, n: int) -> int:
+        self.cache = {}
         return self._climb(n)
 
-    @lru_cache()
     def _climb(self, n: int) -> int:
         if n < 2:
             return 1
+        cache = self.cache.get(n)
+        if cache:
+            return cache
 
-        return self._climb(n - 1) + self._climb(n - 2)
+        res = self._climb(n - 1) + self._climb(n - 2)
+        self.cache[n] = res
+
+        return res
