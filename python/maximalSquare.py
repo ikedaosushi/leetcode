682e8f6c90228ec0edd8204953decb7dc97e3cf4
Ikeda Yutao <yutaro.ikeda@kaizenplatform.com>
2020-02-05 10:58:22 +0900

    maximalSquare

---
+++ python/maximalSquare.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if not matrix or not matrix[0]:
+            return 0
+
+        m, n = len(matrix), len(matrix[0])
+        dp = [[int(e) for e in row] for row in matrix]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] == "1":
+                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
+                else:
+                    dp[i][j] = 0
+        return max([max(row) for row in dp]) ** 2
