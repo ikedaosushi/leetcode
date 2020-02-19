c3c5843591415cc345ccedb3b236158baca92652
Ikeda Yutao <yutaro.ikeda@kaizenplatform.com>
2020-02-19 15:28:06 +0900

    Fix test

--- python/tests/test_addBinary.py
+++ python/tests/test_addBinary.py
@@ -2,7 +2,7 @@ import pytest
 from addBinary import Solution
 
 
-@pytest.mark.parametrize("a,b,expected", [
+@pytest.mark.parametrize("a, b, expected", [
     ("11", "1", "100"),
     ("1010", "1011", "10101"),
     ("0", "1", "1"),
