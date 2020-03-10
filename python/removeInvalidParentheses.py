996f95b8f577772593d272d600117a544c02aadf
Ikeda Yutao <yutaro.ikeda@kaizenplatform.com>
2020-03-10 15:31:09 +0900

    removeInvalidParentheses

--- python/removeInvalidParentheses.py
+++ python/removeInvalidParentheses.py
@@ -2,11 +2,45 @@ from typing import List
 
 
 class Solution:
+    def isValid(self, s) -> bool:
+        stack = 0
+        for lune in s:
+            if lune == "(":
+                stack += 1
+            elif lune == ")":
+                stack -= 1
+                if stack < 0:
+                    return False
+        return stack == 0
+
     def removeInvalidParentheses(self, s: str) -> List[str]:
-        return []
+        if not s:
+            return [""]
+        res = []
+        visited = set()
+        queue = []
 
+        queue.append(s)
+        visited.add(s)
 
-if __name__ == "__main__":
-    string, expected = "()())()", ["()()()", "(())()"]
-    actual = Solution().removeInvalidParentheses(string)
-    print(actual)
+        found = False
+
+        while(queue):
+            s = queue.pop(0)
+            if self.isValid(s):
+                res.append(s)
+                found = True
+            if found:
+                continue
+
+            for i in range(len(s)):
+                if s[i] not in ("(", ")"):
+                    continue
+
+                t = s[:i] + s[i + 1:]
+
+                if t not in visited:
+                    queue.append(t)
+                    visited.add(t)
+
+        return res
--- python/tests/test_removeInvalidParentheses.py
+++ python/tests/test_removeInvalidParentheses.py
@@ -2,12 +2,13 @@ import pytest
 from removeInvalidParentheses import Solution
 
 
-@pytest.mark.skip
 @pytest.mark.parametrize("string,expected", [
     ("()())()", ["()()()", "(())()"]),
+    ("()()))()", ["()()()", "(())()"]),
     ("(a)())()", ["(a)()()", "(a())()"]),
-    (")(", [""])
+    (")(", [""]),
+    ("", [""])
 ])
 def test_removeInvalidParentheses(string, expected):
     actual = Solution().removeInvalidParentheses(string)
-    assert actual == expected
+    assert set(actual) == set(expected)
