140072350c3f6a1034bf7cd9a80039bd2379041d
ikedaosushi <ikeda.yutaro@gmail.com>
2020-06-07 21:41:08 +0900

    rotate

---
+++ interviews/python/rotate.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        if len(matrix) == 0:
+            return
+        matrix.reverse()
+        for i in range(len(matrix)):
+            for j in range(i+1, len(matrix[0])):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
