86421c7074f4d0dda690c65c667a6868cfa4f38e
ikedaosushi <ikeda.yutaro@gmail.com>
2020-06-16 23:21:41 +0900

    mergeTwoLists

---
+++ interviews/python/mergeTwoLists.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        current = res = ListNode()
+        while l1 and l2:
+            if l1.val <= l2.val:
+                current.next = ListNode(l1.val)
+                l1 = l1.next
+            else:
+                current.next = ListNode(l2.val)
+                l2 = l2.next
+            current = current.next
+
+        current.next = l1 if l1 else l2
+
+        return res.next
