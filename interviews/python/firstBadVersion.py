29b575c0848dc489687977074089790b5f4045aa
ikedaosushi <ikeda.yutaro@gmail.com>
2020-06-24 18:29:57 +0900

    firstBadVersion

---
+++ interviews/python/firstBadVersion.py
@@ -0,0 +1,24 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return an integer
+
+
+def isBadVersion(version):
+    pass
+
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        start, end = 1, n
+        while start < end:
+            mid = start + ((end - start) // 2)
+            if isBadVersion(mid):
+                end = mid
+            else:
+                start = mid + 1
+
+        return start
