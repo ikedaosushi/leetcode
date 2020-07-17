b69ba1252fa2f579747a6ca2c9fb3109652ea2dc
ikedaosushi <ikeda.yutaro@gmail.com>
2020-07-17 22:43:04 +0900

    inorderTraversal

---
+++ interviews/python/inorderTraversal.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        stack = []
+        curr = root
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+
+            curr = stack.pop()
+            res.append(curr.val)
+            curr = curr.right
+
+        return res
