3c47bbda356690fabc94b3d198aceafbcfc3905a
ikedaosushi <ikeda.yutaro@gmail.com>
2020-07-21 22:47:42 +0900

    hasPathSum

---
+++ interviews/python/hasPathSum.py
@@ -0,0 +1,16 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if root is None:
+            return False
+
+        if root.left is None and root.right is None and sum == root.val:
+            return True
+
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
