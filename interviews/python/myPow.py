3fc9a55d1b1f1f818dfcea2c603c20976843baf3
ikedaosushi <ikeda.yutaro@gmail.com>
2020-07-30 20:47:25 +0900

    myPow

---
+++ interviews/python/myPow.py
@@ -0,0 +1,10 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if not n:
+            return 1
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+        if n % 2:
+            return x * self.myPow(x, n - 1)
+        else:
+            return self.myPow(x * x, n / 2)
