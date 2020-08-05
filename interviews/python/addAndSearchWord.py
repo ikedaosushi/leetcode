433b20e72bf908e8570756fbe50fb096b751d1ef
ikedaosushi <ikeda.yutaro@gmail.com>
2020-08-05 22:10:56 +0900

    addAndSearchWord

---
+++ interviews/python/addAndSearchWord.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+
+
+class TrieNode():
+    def __init__(self) -> None:
+        self.children = defaultdict(TrieNode)
+        self.isWord = False
+
+
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        """
+        Adds a word into the data structure.
+        """
+        node = self.root
+        for s in word:
+            node = node.children[s]
+        node.isWord = True
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+        """
+        node = self.root
+        self.res = False
+        self.dfs(node, word)
+        return self.res
+
+    def dfs(self, node, word):
+        if not word:
+            if node.isWord:
+                self.res = True
+            return
+
+        if word[0] == ".":
+            for n in node.children.values():
+                self.dfs(n, word[1:])
+        else:
+            node = node.children.get(word[0])
+            if not node:
+                return
+            self.dfs(node, word[1:])
