e70817de1e0afc560b0aa4b4a32f090c303e331b
ikedaosushi <ikeda.yutaro@gmail.com>
2020-08-16 17:51:41 +0900

    maxProfit

---
+++ interviews/python/maxProfit2.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+
+        profits = []
+        max_profit = 0
+        current_min = prices[0]
+        for price in prices:
+            current_min = min(current_min, price)
+            max_profit = max(max_profit, price - current_min)
+            profits.append(max_profit)
+
+        total_max = 0
+        max_profit = 0
+        current_max = prices[-1]
+        for i in range(len(prices) - 1, -1, -1):
+            current_max = max(current_max, prices[i])
+            max_profit = max(max_profit, current_max - prices[i])
+            total_max = max(total_max, max_profit + profits[i])
+
+        return total_max
