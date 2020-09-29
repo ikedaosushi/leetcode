921967aedf025070f1dacece363a8d6ec5f8e1f9
ikedaosushi <ikeda.yutaro@gmail.com>
2020-09-30 00:33:58 +0900

    wordBreak

---
+++ interviews/python/wordBreak.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        ok = [True]
+        for i in range(1, len(s) + 1):
+            ok += any(ok[j] and s[j:i] in wordDict for j in range(i)),
+        return ok[-1]
