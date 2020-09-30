328853c524d889cb4df6d0ece7b9ec8739c3674f
ikedaosushi <ikeda.yutaro@gmail.com>
2020-09-30 22:35:39 +0900

    firstMissingPositive

---
+++ interviews/python/firstMissingPositive.py
@@ -0,0 +1,16 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        size = len(nums)
+        ans = [0] * size
+        for n in nums:
+            if 0 >= n or n > size:
+                continue
+            ans[n-1] = 1
+
+        print(ans)
+        for i, n in enumerate(ans):
+            if n == 0:
+                print(i)
+                return i + 1
+
+        return size + 1
