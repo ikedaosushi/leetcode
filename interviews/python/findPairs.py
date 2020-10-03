6a5c7815753cd0368cc9d437691ca7b6b335eb00
ikedaosushi <ikeda.yutaro@gmail.com>
2020-10-03 18:24:05 +0900

    findPairs

---
+++ interviews/python/findPairs.py
@@ -0,0 +1,13 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def findPairs(self, nums: List[int], k: int) -> int:
+        res = 0
+        c = collections.Counter(nums)
+        for i in c:
+            if k > 0 and i + k in c or k == 0 and c[i] > 1:
+                res += 1
+
+        return res
