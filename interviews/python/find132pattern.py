075464cc39f65205e5dc40f2c7e9157f8ac86f91
ikedaosushi <ikeda.yutaro@gmail.com>
2020-10-24 00:59:52 +0900

    find132pattern

---
+++ interviews/python/find132pattern.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack = []
+        s3 = -float("inf")
+        for n in nums[::-1]:
+            if n < s3:
+                return True
+            while stack and stack[-1] < n:
+                s3 = stack.pop()
+            stack.append(n)
+        return False
