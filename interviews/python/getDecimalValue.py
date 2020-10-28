80771c8393f909bc994792d143d5f9fd6a7af051
ikedaosushi <ikeda.yutaro@gmail.com>
2020-10-28 12:01:52 +0900

    getDecimalValue

---
+++ interviews/python/getDecimalValue.py
@@ -0,0 +1,13 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        ans = 0
+        while head:
+            ans = 2 * ans + head.val
+            head = head.next
+        return ans
