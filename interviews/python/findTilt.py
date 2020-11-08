081dfd75b66b848c111985d8e7b9629a1b5de7f8
ikedaosushi <ikeda.yutaro@gmail.com>
2020-11-08 23:41:39 +0900

    findTilt

---
+++ interviews/python/findTilt.py
@@ -0,0 +1,19 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def findTilt(self, root: TreeNode) -> int:
+        self.ans = 0
+
+        def _sum(node):
+            if not node:
+                return 0
+            left, right = _sum(node.left), _sum(node.right)
+            self.ans += abs(left - right)
+            return node.val + left + right
+        _sum(root)
+        return self.ans
