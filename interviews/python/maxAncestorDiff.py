8cd5aa236f72ca62e738af54d899f1f46c8d6197
ikedaosushi <ikeda.yutaro@gmail.com>
2020-11-09 22:52:41 +0900

    maxAncestorDiff

---
+++ interviews/python/maxAncestorDiff.py
@@ -0,0 +1,19 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxAncestorDiff(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        return self.helper(root, root.val, root.val)
+
+    def helper(self, node, high, low):
+        if not node:
+            return high - low
+        high = max(high, node.val)
+        low = min(low, node.val)
+        return max(self.helper(node.left, high, low), self.helper(node.right, high, low))
