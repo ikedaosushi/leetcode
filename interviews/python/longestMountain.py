67186954920d106f4c1d97ac854259bd27b53e66
ikedaosushi <ikeda.yutaro@gmail.com>
2020-11-16 18:24:21 +0900

    longestMountain

---
+++ interviews/python/longestMountain.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def longestMountain(self, A: List[int]) -> int:
+        res = 0
+        for i in range(1, len(A) - 1):
+            if A[i + 1] < A[i] > A[i - 1]:
+                l = r = i
+                while l and A[l] > A[l-1]:
+                    l -= 1
+                while r + 1 < len(A) and A[r] > A[r + 1]:
+                    r += 1
+                if r - l + 1 > res:
+                    res = r - l + 1
+        return res
